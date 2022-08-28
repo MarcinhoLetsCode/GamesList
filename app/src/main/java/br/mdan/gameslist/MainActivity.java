77a2f746c6a60b1576d162b98c4f99940438b1c4
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-08-28 16:37:48 -0300

    NavigationDrawer Fixed

--- app/src/main/java/br/mdan/gameslist/MainActivity.java
+++ app/src/main/java/br/mdan/gameslist/MainActivity.java
@@ -1,9 +1,12 @@
 package br.mdan.gameslist;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -13,22 +16,26 @@ import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     TabLayout tabLayout;
     ViewPager2 viewPager;
     MaterialToolbar toolbar;
     DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        //setContentView(R.layout.main_layout);
+        setContentView(R.layout.drawer);
 
         tabLayout = findViewById(R.id.tlGame);
         viewPager = findViewById(R.id.vpGames);
-        toolbar = findViewById(R.id.toolBar);
+        toolbar = findViewById(R.id.idToolBar);
         drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nvDrawer);
 
         AdapterFragment adapterFragment = new AdapterFragment(this);
         viewPager.setAdapter(adapterFragment);
@@ -58,17 +65,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        toolbar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "menu", Toast.LENGTH_SHORT).show();
-            }
-        });
+        toolbar.setOnClickListener(view -> Toast.makeText(getApplicationContext(), "menu", Toast.LENGTH_SHORT).show());
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navopen, R.string.navclose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
 }
